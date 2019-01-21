9620b4953d1400e6b0e4d7c01830a412e57588b1
zampierimatteo91 <zampierimatteo.91@gmail.com>
2019-01-21 18:11:01 +0100

    finally made some basic changes

--- day1/ex_1.py
+++ day1/ex_1.py
@@ -3,17 +3,16 @@ import matplotlib.pyplot as mplot
 import numpy as np
 import sys
 
-def f(x):
-	return(x)
-
 xval = np.linspace(-5, 5, num = 101)
 yval = []
 
 if sys.argv[1] == "1":
-	yval = f(xval)
-	print(yval)
+	for i in xval:
+		yval.append(i)
+	
 	mplot.plot(xval, yval)
 	mplot.ylabel('f(x)')
 	mplot.xlabel('x')
+	mplot.title('y = x')
 	mplot.show()
 
