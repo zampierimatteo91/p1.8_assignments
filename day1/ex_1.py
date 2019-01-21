7c83420c0751493ae9a1c5806c59b3e955de1714
zampierimatteo91 <zampierimatteo.91@gmail.com>
2019-01-21 18:38:04 +0100

    added exp(x) and sqrt(|x|) functions

--- day1/ex_1.py
+++ day1/ex_1.py
@@ -2,13 +2,16 @@
 import matplotlib.pyplot as mplot
 import numpy as np
 import sys
+import math
 
 xval = np.linspace(-5, 5, num = 101)
 yval = []
 
 if not sys.argv[1:]:
 	print("usage: ex_1.py [function_number]\n\nFunction:\n\
-		(1) -> f(x) = x\n")
+		(1) -> f(x) = x\n\
+		(2) -> f(x) = exp(x)\n\
+		(3) -> f(x) = sqrt(|x|)\n")
 	exit(1)
 
 elif sys.argv[1] == "1":
@@ -21,3 +24,22 @@ elif sys.argv[1] == "1":
 	mplot.title('y = x')
 	mplot.show()
 
+elif sys.argv[1] == "2":
+	for i in xval:
+		yval.append(math.exp(i))
+	
+	mplot.plot(xval, yval)
+	mplot.ylabel('f(x)')
+	mplot.xlabel('x')
+	mplot.title('y = exp(x)')
+	mplot.show()
+
+elif sys.argv[1] == "3":
+	for i in xval:
+		yval.append(math.sqrt(abs(i)))
+	
+	mplot.plot(xval, yval)
+	mplot.ylabel('f(x)')
+	mplot.xlabel('x')
+	mplot.title('y = sqrt(|x|)')
+	mplot.show()
