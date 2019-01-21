4581cc295c07c7ea8ace6ea46ba65924cec91895
zampierimatteo91 <zampierimatteo.91@gmail.com>
2019-01-21 18:09:09 +0100

    added f2=sin(x), f3=cos(x), f4=tan(x)

--- day1/ex_1.py
+++ day1/ex_1.py
@@ -2,18 +2,47 @@
 import matplotlib.pyplot as mplot
 import numpy as np
 import sys
-
-def f(x):
-	return(x)
+import math
 
 xval = np.linspace(-5, 5, num = 101)
 yval = []
 
 if sys.argv[1] == "1":
-	yval = f(xval)
-	print(yval)
+	for i in xval:
+		yval.append(i)
+	
 	mplot.plot(xval, yval)
 	mplot.ylabel('f(x)')
 	mplot.xlabel('x')
+	mplot.title('y = x')
 	mplot.show()
 
+elif sys.argv[1] == "2":
+	for i in xval:
+		yval.append(math.sin(i))
+
+	mplot.plot(xval, yval)
+	mplot.ylabel('f(x)')
+	mplot.xlabel('x')
+	mplot.title('y = sin(x)')
+	mplot.show()
+
+elif sys.argv[1] == "3":
+	for i in xval:
+		yval.append(math.cos(i))
+
+	mplot.plot(xval, yval)
+	mplot.ylabel('f(x)')
+	mplot.xlabel('x')
+	mplot.title('y = cos(x)')
+	mplot.show()
+
+elif sys.argv[1] == "4":
+	for i in xval:
+		yval.append(math.tan(i))
+
+	mplot.plot(xval, yval)
+	mplot.ylabel('f(x)')
+	mplot.xlabel('x')
+	mplot.title('y = tan(x)')
+	mplot.show()
